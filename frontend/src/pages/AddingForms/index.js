5b08974d6eb71c173b25bd2bafde02066a8d3414
somin18501 <somingandhi@gmail.com>
2023-08-28 13:29:19 +0530

    bug fixes

--- frontend/src/pages/AddingForms/index.js
+++
@@ -1,5 +0,0 @@
-import AddHospital from "./addHospital";
-import AddLaboratory from "./addLab";
-import AddMedicalStore from "./addMedical";
-
-export {AddHospital,AddLaboratory,AddMedicalStore};
--- frontend/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-// home pages
-import HomePage from "./HomePage/HomePage";
-import LoginPage from "./Auth/LoginPage";
-import AllocationPage from "./AllocationPage/AllocationPage";
-
-export { HomePage, LoginPage, AllocationPage };
